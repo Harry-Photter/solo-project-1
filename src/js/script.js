a262952b34e7df77925b2864699ac79ccce5d75b
Mateusz Furman <mateuszfurman.nf@gmail.com>
2020-12-02 11:34:01 +0100

    Add overlay

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,18 @@
 
 const button = document.querySelector('.hamburger');
 const sidenav = document.querySelector('.sidenav');
+const overlay = document.querySelector('.overlay');
+
 
 button.addEventListener('click',
   function () {
     sidenav.classList.toggle('show');
     button.classList.toggle('show');
+    overlay.classList.toggle('show');
   });
+
+overlay.addEventListener('click', function () {
+  sidenav.classList.remove('show');
+  overlay.classList.remove('show');
+  button.classList.toggle('show');
+});
